fc12ade416f007829c0d82d5ae88750f96f19c61
R4Vphp <rafal.kosmala@vp.pl>
2024-07-04 20:10:24 +0200

    chapter 8 finish

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,17 +16,20 @@ const data = await sql<LatestInvoiceRaw>`
   ORDER BY invoices.date DESC
   LIMIT 5`;
 
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
